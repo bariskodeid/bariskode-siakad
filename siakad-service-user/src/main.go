0690fabae9ff2de1ceb5b8d42ff1cc511c95f4a1
Kucing Sakti <bariskode24@gmail.com>
2025-04-21 07:34:19 +0700

    feat: update go.mod and go.sum for new dependencies; modify .air.toml for build configuration; enhance main.go structure

--- siakad-service-user/src/main.go
+++ siakad-service-user/src/main.go
@@ -4,32 +4,33 @@ import (
 	"log"
 
 	"github.com/bariskodeid/bariskode-siakad/siakad-service-user/src/config"
+	"github.com/bariskodeid/bariskode-siakad/siakad-service-user/src/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    config.LoadConfig()
+	config.LoadConfig()
 
-    if config.AppConfig.AppDebugMode == "true" {
-        gin.SetMode(gin.DebugMode)
-    } else {
-        gin.SetMode(gin.ReleaseMode)
-    }
+	if config.AppConfig.AppDebugMode == "true" {
+		gin.SetMode(gin.DebugMode)
+	} else {
+		gin.SetMode(gin.ReleaseMode)
+	}
 
-    err := config.InitDB()
-    if err != nil {
-        log.Fatal("failed to connect to database: ", err)
-    }
-    config.DB.AutoMigrate(&models.User{})
+	err := config.InitDB()
+	if err != nil {
+		log.Fatal("failed to connect to database: ", err)
+	}
+	config.DB.AutoMigrate(&models.User{})
 
-    userRepo := repositories.NewUserRepository(config.DB)
+	// userRepo := repositories.NewUserRepository(config.DB)
 
-    r := gin.Default()
-    r.Use(gin.Recovery())
-    // routes.RegisterRoutes(r, userRepo)
+	r := gin.Default()
+	r.Use(gin.Recovery())
+	// routes.RegisterRoutes(r, userRepo)
 
-    log.Printf("Auth service running on port %s", config.AppConfig.AppPort)
-    if err := r.Run(":" + config.AppConfig.AppPort); err != nil {
-        log.Fatal("failed to run server: ", err)
-    }
+	log.Printf("Auth service running on port %s", config.AppConfig.AppPort)
+	if err := r.Run(":" + config.AppConfig.AppPort); err != nil {
+		log.Fatal("failed to run server: ", err)
+	}
 }
